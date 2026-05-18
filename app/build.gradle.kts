d4d71b6bf2cee3efd34d2b462a13b5d1f22ed3d7
guolei <leilei4365@gmail.com>
2026-05-18 12:20:46 +0800

    Bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.virb.lite"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
